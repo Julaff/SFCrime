8d2485f47a26e15e39facc6f23db434fc09e8ed1
Julien Laffargue <julien.laffargue@gmail.com>
2016-02-26 15:11:41 +0100

    split Kaggle to one repo per competition

---
+++ SFCrime.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+train = pd.read_csv('train.csv')
+#test = pd.read_csv('test.csv')
+#sampleSub = pd.read_csv('sampleSubmission.csv')
+
+print(train[:5])
+#print(test[:5])
+#print(sampleSub[:5])
+
+print("hi")
